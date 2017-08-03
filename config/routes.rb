9b43c84b167cf962c1391e41d2e60aede8df236c
Steve <steve@smartfame.co.ke>
2017-08-03 19:58:12 +0300

    Add Sidekiq Web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'sidekiq/web'
+
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   post 'sms_notifications/start'
