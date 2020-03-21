bdeac7e823b37f18edae3deced5d7c906c11bbba
Steve <steve@smartfame.co.ke>
2020-03-21 23:21:10 +0300

    Update Sidekiq config to avoid Redis conflicts with older app's

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-    config.redis = { namespace: 'safari_gate', url: 'redis://localhost:6379/13' }
+    config.redis = { namespace: 'safaricomg8way', url: 'redis://localhost:6379/17' }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { namespace: 'safari_gate', url: 'redis://localhost:6379/13' }
+    config.redis = { namespace: 'safaricomg8way', url: 'redis://localhost:6379/17' }
 end
