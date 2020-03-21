c197925652705363c334bad3bee0c1e130bc96a5
Steve <steve@smartfame.co.ke>
2020-03-21 15:39:26 +0300

    Add Reason to Subscriptions for real

--- db/migrate/20200321123702_add_reason_to_subscriptions.rb
+++ db/migrate/20200321123702_add_reason_to_subscriptions.rb
@@ -1,5 +1,5 @@
 class AddReasonToSubscriptions < ActiveRecord::Migration[6.0]
   def change
-    add_column :subscriptions, :type, :string
+    add_column :subscriptions, :reason, :string
   end
 end
